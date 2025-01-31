0c7e4c71bf18ac7ef8ba581f582a3ee7d57839bb
2409akshay <2409akshay@gmail.com>
2025-01-31 19:48:46 +0530

    Add files via upload

---
+++ BrowserProfileBackup.ps1
@@ -0,0 +1,41 @@
+#Bookmark paths
+$chromeProfiles = Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data" | Where-Object { $_.Name -like "Default" -or $_.Name -like "*Profile*"}
+$edgeProfiles = Get-ChildItem -Path $env:LOCALAPPDATA"\Microsoft\Edge\User Data" | Where-Object { $_.Name -like "Default" -or $_.Name -like "*Profile*"}
+
+#Output path
+$outputPath = $($env:USERPROFILE + "\OneDrive\Important_Stuff\Bookmarks\")
+
+
+#copy Chrome Bookmarks
+foreach ($profile in $chromeProfiles)
+{
+	Get-ChildItem $profile.FullName -Filter "Bookmark*" | Foreach-Object {
+		Copy-Item -Path $_.PSPath -Destination $($outputPath + $profile.Name + "_Chrome_" + $(Get-Date).ToString("MMddyyyy") + "_" + $_.Name)
+	}
+}
+
+#Copy Edge Bookmarks
+foreach ($profile in $edgeProfiles)
+{
+	Get-ChildItem $profile.FullName -Filter "Bookmark*" | Foreach-Object {
+		Copy-Item -Path $_.PSPath -Destination $($outputPath + $profile.Name + "_Edge_" + $(Get-Date).ToString("MMddyyyy") + "_" + $_.Name)
+	}
+}
+
+#Get Chrome Profile mapping
+$chromeProfileMapping = Get-ChildItem $env:LOCALAPPDATA"\Google\Chrome\User Data" -Filter "Local State"
+$profileMappingObj = Get-Content $chromeProfileMapping.FullName | ConvertFrom-Json
+#Add empty line in output file
+"" > $($outputPath + "ChromeProfileMapping.txt")
+$profileMappingObj.profile.profiles_order | Foreach-Object { 
+	$_ + " : " + $profileMappingObj.profile.info_cache.$_.name >> $($outputPath + "ChromeProfileMapping.txt")
+}
+
+#Get Edge Profile mapping
+$edgeProfileMapping = Get-ChildItem $env:LOCALAPPDATA"\Microsoft\Edge\User Data" -Filter "Local State"
+$profileMappingObj = Get-Content $edgeProfileMapping.FullName | ConvertFrom-Json
+#Add empty line in output file
+"" > $($outputPath + "EdgeProfileMapping.txt")
+$profileMappingObj.profile.profiles_order | Foreach-Object { 
+	$_ + " : " + $profileMappingObj.profile.info_cache.$_.name >> $($outputPath + "EdgeProfileMapping.txt")
+}
